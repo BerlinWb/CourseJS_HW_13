d19ea892462f1d4dd4495319537ff57b69f738ed
Oleh Svetlov <95508050+BerlinWb@users.noreply.github.com>
2022-07-14 18:52:27 +0300

    Add files via upload

---
+++ Course_Js_Task13/js/script.js
@@ -0,0 +1,68 @@
+"use strict";
+
+// 1
+//let x = 10, y = 7;
+//let result = (x > y) ? alert("x больше, чем y") : alert("x не больше, чем y");
+
+// 2
+/*
+let num = +prompt("Enter the number")
+
+if (num % 2 === 0 ) {
+  alert("Even" + num + "Number")
+} else {
+  alert("odd" + num + "Number")
+}
+*/
+
+// 3
+/*
+let a = prompt("Enter the number"); 
+
+if (Math.sign(a) === 1) {
+  console.log("«Number»" + a + "«Positive»");
+  console.log("Number of digits:", a.length);
+}else if(!!Math.sign(a)) {
+  console.log("«Number»" + a + "«Negative»");
+  console.log("Number of digits:", a.length);
+}
+*/
+
+// 4
+/*
+const num1 = prompt("Enter first number: ");
+const num2 = prompt("Enter second number: ");
+const num3 = prompt("Enter third number: ");
+let max;
+
+if(num1 >= num2 && num1 >= num3) {
+    max = num1;
+}
+else if (num2 >= num1 && num2 >= num3) {
+    max = num2;
+}
+else {
+    max = num3;
+}
+alert("Maximum number is " + max);
+*/
+
+// 5
+/*
+function check(a, b, c)
+{
+    if (a + b <= c || a + c <= b || b + c <= a)
+        return false;
+    else
+        return true;
+}
+ 
+let a = +prompt("Enter first number");
+let b = +prompt("Enter second number");
+let c = +prompt("Enter third number");
+    if (check(a, b, c))
+        console.log("Valid");
+    else
+        console.log("Invalid"); 
+
+    */
